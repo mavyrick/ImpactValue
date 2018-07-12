412aa4cdb807f40bd7cb4f54c93efdddcf5a781b
Josh Sorokin <jrsorokin@gmail.com>
2018-07-12 22:59:05 +0300

    Initial Commit

---
+++ ImpactValue/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  ImpactValue
+//
+//  Created by Josh Sorokin on 12/07/2018.
+//  Copyright © 2018 Josh Sorokin. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ImpactValue/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  ImpactValue
+//
+//  Created by Josh Sorokin on 12/07/2018.
+//  Copyright © 2018 Josh Sorokin. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
